2b9ecb12b10c9b67449a1e541f198fc740587230
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-05 17:37:08 -0400

    finalizado projeto sem interface

---
+++ ProblemaSemInterface/Entitites/CarRental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ProblemaSemInterface.Entitites
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Veiculo Veiculo { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Veiculo veiculo)
+        {
+            Start = start;
+            Finish = finish;
+            Veiculo = veiculo;
+        }
+
+
+    }
+}
---
+++ ProblemaSemInterface/Entitites/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemaSemInterface.Entitites
+{
+    class Veiculo
+    {
+        public string Modelo { get; set; }
+
+        public Veiculo(string modelo)
+        {
+            Modelo = modelo;
+        }
+    }
+}
