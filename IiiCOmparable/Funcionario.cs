659c9d800d12be24442986e8996c1ca64237d8f4
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-08 10:55:47 -0400

    Mexendo com a interface IComparable

---
+++ IiiCOmparable/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IiiCOmparable
+{
+    class Funcionario : IComparable
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string n)
+        {
+            string[] x = n.Split(',');
+            Nome = x[0];
+            Salario = double.Parse(x[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome
+                + "\nSalario: " + Salario.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Funcionario");
+            }
+            Funcionario other = obj as Funcionario;
+            return Salario.CompareTo(other.Salario);
+        }
+    }
+}
---
+++ IiiCOmparable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IiiCOmparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = @"C:\Projetos\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(caminho))
+                {
+
+                    List<Funcionario> f = new List<Funcionario>();
+                    
+                    while (!sr.EndOfStream)
+                    {
+                        f.Add(new Funcionario(sr.ReadLine()));
+                    }
+
+                    f.Sort();
+                    
+                    foreach (var str in f)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            
+
+            }catch(IOException e)
+            {
+                Console.WriteLine("Houve um erro");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
